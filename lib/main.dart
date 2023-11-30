ebf9688cb1a4c5bde560218f635b589a4763f194
Jeel <itsjeel01@gmail.com>
2023-11-30 23:51:05 +0530

    Replaced app theme Lato to Inter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:voxify/providers/theme_state_manager.dart';
 import 'package:voxify/screens/onboarding.dart';
-import '../values/theme.dart' as custom_theme;
 
 void main() {
   runApp(const MyApp());
@@ -11,16 +12,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    custom_theme.Theme(context);
+    return ChangeNotifierProvider(
+      create: (context) => ThemeStateManager(context),
+      builder: (context, child) {
+        var themeStateManager = Provider.of<ThemeStateManager>(context);
 
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: custom_theme.Theme.lightTheme,
-      darkTheme: custom_theme.Theme.darkTheme,
-      themeMode: ThemeMode.system,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const Onboarding(),
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: themeStateManager.currentTheme,
+          themeMode: ThemeMode.system,
+          initialRoute: '/',
+          routes: {
+            '/': (context) => const Onboarding(),
+          },
+        );
       },
     );
   }
--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
+import 'package:voxify/providers/theme_state_manager.dart';
 import '../methods/ui_methods.dart';
-import '../values/theme.dart' as custom_theme;
 
 class Onboarding extends StatelessWidget {
-  const Onboarding({super.key});
+  const Onboarding({Key? key}) : super(key: key);
 
   final String heading = 'Welcome to Voxify!';
   final String subheading =
@@ -12,78 +13,81 @@ class Onboarding extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width = custom_theme.Theme.width;
-    var height = custom_theme.Theme.height;
-
-    final String assetName = Theme.of(context).brightness == Brightness.dark
-        ? 'assets/images/voxify-dark.svg'
-        : 'assets/images/voxify-light.svg';
-
     UIMethods.setStatusBarColor(context);
 
     return SafeArea(
-        child: Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 0.04 * height, horizontal: 0.08 * width),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              SizedBox(
-                width: double.infinity,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                        vertical: height * 0.01,
-                        horizontal: 0,
+      child: Consumer<ThemeStateManager>(
+        builder: (context, themeStateManager, _) {
+          var width = themeStateManager.width;
+          var height = themeStateManager.height;
+
+          final String assetName = Theme.of(context).brightness == Brightness.dark
+              ? 'assets/images/voxify-dark.svg'
+              : 'assets/images/voxify-light.svg';
+
+          return Scaffold(
+            body: Padding(
+              padding: EdgeInsets.symmetric(vertical: 0.04 * height, horizontal: 0.08 * width),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: height * 0.01,
+                            horizontal: 0,
+                          ),
+                          child: SvgPicture.asset(assetName, width: width * 0.1),
+                        ),
+                        SizedBox(
+                          width: 0.02 * width,
+                        ),
+                        Text(
+                          'Voxify',
+                          style: Theme.of(context).textTheme.displayLarge,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SvgPicture.asset(
+                    'assets/images/onboarding.svg',
+                    height: 0.35 * height,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        heading,
+                        style: Theme.of(context).textTheme.displayMedium,
                       ),
-                      child: SvgPicture.asset(assetName, width: width * 0.1),
-                    ),
-                    SizedBox(
-                      width: 0.02 * width,
-                    ),
-                    Text(
-                      'Voxify',
-                      style: Theme.of(context).textTheme.displayLarge,
-                    ),
-                  ],
-                ),
-              ),
-              SvgPicture.asset(
-                'assets/images/onboarding.svg',
-                height: 0.35 * height,
-              ),
-              Column(
-                children: [
-                  Text(
-                    heading,
-                    style: Theme.of(context).textTheme.displayMedium,
+                      SizedBox(
+                        height: height * 0.01,
+                      ),
+                      Text(
+                        subheading,
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.bodySmall,
+                      ),
+                    ],
                   ),
-                  SizedBox(
-                    height: height * 0.01,
-                  ),
-                  Text(
-                    subheading,
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.bodySmall,
+                  Column(
+                    children: [
+                      UIMethods.getFilledButton(() => null, 'Register'),
+                      SizedBox(
+                        height: height * 0.02,
+                      ),
+                      UIMethods.getFilledButton(() => null, 'Sign In'),
+                    ],
                   ),
                 ],
               ),
-              Column(
-                children: [
-                  UIMethods.getFilledButton(() => null, 'Register'),
-                  SizedBox(
-                    height: height * 0.02,
-                  ),
-                  UIMethods.getFilledButton(() => null, 'Sign In'),
-                ],
-              ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
-    ));
+    );
   }
 }
